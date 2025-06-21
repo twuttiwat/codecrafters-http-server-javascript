2179ac0e14ee21684e480effd8ea44701eabc860
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2025-06-21 13:06:20 +0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -2,6 +2,8 @@ const net = require("net");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.end('HTTP/1.1 200 OK\r\n\r\n')
+
   socket.on("close", () => {
     socket.end();
   });
