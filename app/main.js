7e05a0f5ccc5a0024baeea99ad53dc660ccf61d2
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2025-08-11 21:14:09 +0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -2,7 +2,21 @@ const net = require("net");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.end('HTTP/1.1 200 OK\r\n\r\n')
+
+  socket.on('data', (data) => {
+    console.log(`Receive data ${data.toString()}`)
+
+    let reqLine = data.toString().split("\r\n")[0]
+    console.log(`request line: ${reqLine}`)
+
+    let reqTarget = reqLine.split(' ')[1]
+    console.log(`request target: ${reqTarget}`)
+
+    if (reqTarget === '/')
+      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    else
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+  });
 
   socket.on("close", () => {
     socket.end();
