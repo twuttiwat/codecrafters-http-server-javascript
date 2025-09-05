9d2ee3b324be7c1f3e9e212756db028b59f49228
Teerawat Wuttiwat <781147+twuttiwat@users.noreply.github.com>
2025-09-05 18:45:27 +0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -6,11 +6,17 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     console.log(`Receive data ${data.toString()}`)
 
-    let reqLine = data.toString().split("\r\n")[0]
+    //console.log(data.toString().split("\r\n"))
+
+    const [reqLine, ...headers] = data.toString().split("\r\n");
+    
+    //let reqLine = data.toString().split("\r\n")[0]
     console.log(`request line: ${reqLine}`)
+    console.log(`headers: ${headers}`)
 
     let reqTarget = reqLine.split(' ')[1]
     console.log(`request target: ${reqTarget}`)
+
 
     const endpoints = reqTarget.split("/");
     console.log('endpoints:', endpoints);
@@ -22,7 +28,12 @@ const server = net.createServer((socket) => {
       const result = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoStr.length}\r\n\r\n${echoStr}`;
       console.log(result);
       socket.write(result);
-
+    } else if (endpoints[1] === "user-agent") {
+      const userAgentHeader = headers.find( header => header.startsWith("User-Agent:"));
+      const userAgent = userAgentHeader.split(":")[1].trim();
+      const result = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+      
+      socket.write(result);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
     }
